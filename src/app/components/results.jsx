b6e42499e40da0b8e52640d807bbbaaced130d54
Francisco Pantoja <fcopantoja@gmail.com>
2015-09-25 14:58:45 -0300

    :..

--- src/app/components/results.jsx
+++ src/app/components/results.jsx
@@ -2,24 +2,18 @@ let React = require('react');
 let mui = require('material-ui');
 let ThemeManager = new mui.Styles.ThemeManager();
 let Colors = mui.Styles.Colors;
-let Paper = mui.Paper;
-let RaisedButton = mui.RaisedButton;
 
-let ReactRouter = require('react-router')
-let Router = ReactRouter.Router
-let Route = ReactRouter.Route
-let FloatingActionButton = mui.FloatingActionButton
 
-let Card = mui.Card
-let CardHeader = mui.CardHeader
-let CardMedia = mui.CardMedia
-let CardTitle = mui.CardTitle
+let Paper = mui.Paper
+let Table = mui.Table
+let TableHeader = mui.TableHeader
+let TableRow = mui.TableRow
+let TableHeaderColumn = mui.TableHeaderColumn
+let TableBody = mui.TableBody
+let TableRowColumn = mui.TableRowColumn
+let TableFooter = mui.TableFooter
 let Avatar = mui.Avatar
-let CardText = mui.CardText
 let FontIcon = mui.FontIcon
-let TextField = mui.TextField
-let RadioButton = mui.RadioButton
-let RadioButtonGroup = mui.RadioButtonGroup
 
 let Results = React.createClass({
   contextTypes: {
@@ -27,7 +21,36 @@ let Results = React.createClass({
   },
 
   getInitialState: function() {
-    return null
+    return {
+      fixedHeader: true,
+      fixedFooter: true,
+      stripedRows: false,
+      showRowHover: false,
+      selectable: false,
+      multiSelectable: false,
+      enableSelectAll: false,
+      deselectOnClickaway: false,
+      height: '400px',
+    };
+  },
+
+  getDefaultProps() {
+    return {
+      cards:[
+        {'right': 12, type: 'number', 'answer': 12},
+        {'right': 3, type: 'line', 'answer': 4},
+        {'right': 4, type: 'line', 'answer': 5},
+        {'right': 12, type: 'number', 'answer': 12},
+        {'right': 3, type: 'line', 'answer': 4},
+        {'right': 4, type: 'line', 'answer': 5},
+        {'right': 12, type: 'number', 'answer': 12},
+        {'right': 3, type: 'line', 'answer': 4},
+        {'right': 4, type: 'line', 'answer': 5},
+        {'right': 12, type: 'number', 'answer': 12},
+        {'right': 3, type: 'line', 'answer': 4},
+        {'right': 4, type: 'line', 'answer': 5},
+      ]
+    }
   },
 
   childContextTypes: {
@@ -55,33 +78,51 @@ let Results = React.createClass({
       
     }
 
+    /*{card.answer === card.right &&
+            <Avatar icon={<FontIcon className="fa fa-check" />} backgroundColor={Colors.blue400}/>}
+          </TableRowColumn>*/
+
+    let rows = this.props.cards.map(function(card, index) {
+      return (
+        <TableRow key={index}>
+          <TableRowColumn>{index + 1}</TableRowColumn>
+          <TableRowColumn>{card.right}</TableRowColumn>
+          <TableRowColumn>{card.answer}</TableRowColumn>
+          <TableRowColumn>{card.type}</TableRowColumn>
+          <TableRowColumn>2323</TableRowColumn>
+        </TableRow>
+      )
+    })
+
     return (
-      <div>
-      <Card style={cardStyle}>
-        <CardHeader
-          title="Resultados"
-          subtitle="Ishihara Color Vision Test"
-          avatar={<Avatar icon={<FontIcon className="fa fa-eye" />}/>}/>
-      </Card>
-      <Card style={cardStyle}>
-        <CardHeader
-          title="Resultados"
-          subtitle="Ishihara Color Vision Test"
-          avatar={<Avatar icon={<FontIcon className="fa fa-eye" />}/>}/>
-      </Card>
-      <Card style={cardStyle}>
-        <CardHeader
-          title="Resultados"
-          subtitle="Ishihara Color Vision Test"
-          avatar={<Avatar icon={<FontIcon className="fa fa-eye" />}/>}/>
-      </Card>
-      <Card style={cardStyle}>
-        <CardHeader
-          title="Resultados"
-          subtitle="Ishihara Color Vision Test"
-          avatar={<Avatar icon={<FontIcon className="fa fa-eye" />}/>}/>
-      </Card>
-      </div>
+      <Paper style={cardStyle} zDepth={2}>
+      <Table
+        height={this.state.height}
+        fixedHeader={this.state.fixedHeader}
+        fixedFooter={this.state.fixedFooter}
+        selectable={this.state.selectable}>
+        <TableHeader enableSelectAll={this.state.enableSelectAll}>
+          <TableRow>
+            <TableHeaderColumn colSpan="3" tooltip='Super Header' style={{textAlign: 'center'}} style={{backgroundColor:'#00bcd4', width:'100%', color:'white'}}>
+              <h2>Your Results</h2>
+            </TableHeaderColumn>
+          </TableRow>
+          <TableRow>
+            <TableHeaderColumn tooltip='The Number'>Number</TableHeaderColumn>
+            <TableHeaderColumn tooltip='The Answer'>Answer</TableHeaderColumn>
+            <TableHeaderColumn tooltip='The RIght Answer'>Right</TableHeaderColumn>
+            <TableHeaderColumn tooltip='The Type'>Type</TableHeaderColumn>
+            <TableHeaderColumn tooltip='Correct/Wrong'>Correct/Wrong</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody
+          deselectOnClickaway={this.state.deselectOnClickaway}
+          showRowHover={this.state.showRowHover}
+          stripedRows={this.state.stripedRows}>
+          {rows}
+        </TableBody>
+      </Table>
+      </Paper>
     
     );
   }
